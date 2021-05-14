4cc334ba941ee578c3b6896b59df2ee24ffd2e97
JasonVolkoff <kashijason@gmail.com>
2021-05-14 05:46:09 -0700

    Added static & templates; started routes for 2 of 4 templates

--- book_authors_proj/books_authors_app/urls.py
+++ book_authors_proj/books_authors_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.add_book),
-    path('authors', views.add_author),
+    path('', views.books),
+    path('add_book', views.add_book),
+    path('authors', views.authors),
+    path('add_author', views.add_author),
     path('authors/<int:id>', views.see_author),
     path('books/<int:id>', views.see_book)
 ]
--- book_authors_proj/books_authors_app/views.py
+++ book_authors_proj/books_authors_app/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import Book, Author
 # Create your views here.
+
+
+def books(request):
+    context = {
+        "all_books": Book.objects.all()
+    }
+    return render(request, 'add_book.html', context)
+
+
+def add_book(request):
+    if request.method == "POST":
+        Book.objects.create(
+            title=request.POST['title'],
+            desc=request.POST['description']
+        )
+    return redirect('/')
+
+
+def authors(request):
+    return render(request, 'add_author.html')
+
+
+def add_author(request):
+    if request.method == "POST":
+        Author.objects.create(
+            first_name=request.POST['firstName'],
+            last_name=request.POST['lastName'],
+            notes=request.POST['notes'],
+            books=request.POST['books']
+        )
+    return redirect('/authors')
+
+
+def see_author(request, id):
+    return render(request, 'see_author.html')
+
+
+def see_book(request, id):
+    context = {
+        "book": Book.objects.get(id=id),
+        "authors": Author.objects.all()
+    }
+    return render(request, 'see_book.html', context)
